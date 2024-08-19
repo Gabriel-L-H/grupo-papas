6e64362035dcfa5e5d2dc18ca8fbae202bff3628
Gabriel Leme Horacio <lemehoraciogabriel@gmail.com>
2024-08-18 23:25:33 -0300

    Implementação de background, melhor posicionamento de elementos e da tela de login, mudança de cores e testando o js

---
+++ Cadastro/Login.js
@@ -0,0 +1,7 @@
+function teste(){
+    var a = document.getElementsByTagName("label");
+    for(i = 0; i < a.length; i++){
+    a[i].style.color = "black";
+    a[i].style.bottom = 20 + "px";
+    }
+};
