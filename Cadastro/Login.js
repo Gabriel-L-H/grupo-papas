6a7c968275c5095f45bf489a2f8ae606c83ed5ef
Gabriel Leme Horacio <lemehoraciogabriel@gmail.com>
2024-08-29 23:08:40 -0300

    adicionando animação dos labels de email e senha, além de adicionar defer ao js no documento html

--- Cadastro/Login.js
+++ Cadastro/Login.js
@@ -1,7 +1,87 @@
-function teste(){
-    var a = document.getElementsByTagName("label");
-    for(i = 0; i < a.length; i++){
-    a[i].style.color = "black";
-    a[i].style.bottom = 20 + "px";
+//animação do label de Email ao estar em focus//
+var id = null
+function animEmail(){
+    document.getElementById("text-email").style.color = "blue";
+
+    var p = 0
+    clearInterval(id)
+    id = setInterval(frame, 3)
+    function frame(){
+        if (p == 30){
+            clearInterval(id)
+        }
+
+        else{
+            p++
+            document.getElementById("text-email").style.bottom = p + "px"
+        }
     }
 };
+
+//animação do label de Email ao estar em blur//
+function animEmailCancel(){
+    document.getElementById("text-email").style.color = "white";
+
+    var p = 30
+    clearInterval(id)
+    id = setInterval(frame, 3)
+    function frame(){
+        if (p == 3){
+            clearInterval(id)
+        }
+
+        else{
+            p--
+            document.getElementById("text-email").style.bottom = p + "px"
+        }
+    }
+};
+
+//animação do label de senha ao estar em focus//
+function animSenha(){
+    document.getElementById("text-senha").style.color = "blue";
+
+    var p = 0
+    clearInterval(id)
+    id = setInterval(frame, 3)
+    function frame(){
+        if (p == 30){
+            clearInterval(id)
+        }
+
+        else{
+            p++
+            document.getElementById("text-senha").style.bottom = p + "px"
+        }
+    }
+};
+
+//animação do label de senha ao estar em blur//
+function animSenhaCancel(){
+    document.getElementById("text-senha").style.color = "white";
+
+    var p = 30
+    clearInterval(id)
+    id = setInterval(frame, 3)
+    function frame(){
+        if (p == 3){
+            clearInterval(id)
+        }
+
+        else{
+            p--
+            document.getElementById("text-senha").style.bottom = p + "px"
+        }
+    }
+};
+
+//convertendo label email e senha para variaveis
+var elementEmail = document.getElementById("email");
+var elementSenha = document.getElementById("senha");
+
+//chamando as funções de animação do email e senha ao estar em focus ou blur//
+elementEmail.addEventListener("focus", animEmail);
+elementEmail.addEventListener("blur", animEmailCancel);
+
+elementSenha.addEventListener("focus", animSenha);
+elementSenha.addEventListener("blur", animSenhaCancel);
