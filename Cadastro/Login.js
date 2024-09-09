231d4111b58b2f20d1ad51e312d6f46f3ecbcb2c
Gabriel Leme Horacio <lemehoraciogabriel@gmail.com>
2024-09-09 17:43:29 -0300

    implementando animação do papas e animação do cadastro

--- Cadastro/Login.js
+++ Cadastro/Login.js
@@ -1,8 +1,8 @@
 //animação do label de Email ao estar em focus//
-
+ 
 function animEmail(){
     document.getElementById("text-email").style.color = "blue";
-
+ 
     var p = 0
     clearInterval(id)
     var id = setInterval(frame, 3)
@@ -10,22 +10,19 @@ function animEmail(){
         if (p == 30){
             clearInterval(id)
         }
-
+ 
         else{
             p++
             document.getElementById("text-email").style.bottom = p + "px"
         }
     }
 };
-
-
-
-
+ 
 
 //animação do label de Email ao estar em blur//
 function animEmailCancel(){
     document.getElementById("text-email").style.color = "white";
-
+ 
     var p = 30
     clearInterval(id2)
     var id2 = setInterval(frame, 3)
@@ -33,21 +30,19 @@ function animEmailCancel(){
         if (p == 3){
             clearInterval(id2)
         }
-
+ 
         else{
             p--
             document.getElementById("text-email").style.bottom = p + "px"
         }
     }
 };
-
-
-
+ 
 
 //animação do label de senha ao estar em focus//
 function animSenha(){
     document.getElementById("text-senha").style.color = "blue";
-
+ 
     var p = 0
     clearInterval(id3)
     var id3 = setInterval(frame, 3)
@@ -55,22 +50,19 @@ function animSenha(){
         if (p == 30){
             clearInterval(id3)
         }
-
+ 
         else{
             p++
             document.getElementById("text-senha").style.bottom = p + "px"
         }
     }
 };
-
-
-
-
+ 
 
 //animação do label de senha ao estar em blur//
 function animSenhaCancel(){
     document.getElementById("text-senha").style.color = "white";
-
+ 
     var p = 30
     clearInterval(id4)
     var id4 = setInterval(frame, 3)
@@ -78,63 +70,113 @@ function animSenhaCancel(){
         if (p == 3){
             clearInterval(id4)
         }
-
+ 
         else{
             p--
             document.getElementById("text-senha").style.bottom = p + "px"
         }
     }
 };
-
+ 
 //convertendo label email e senha para variaveis
 var elementEmail = document.getElementById("email");
 var elementSenha = document.getElementById("senha");
-
+ 
 //chamando as funções de animação do email e senha ao estar em focus ou blur//
 elementEmail.addEventListener("focus", animEmail);
 elementEmail.addEventListener("blur", animEmailCancel);
-
+ 
 elementSenha.addEventListener("focus", animSenha);
 elementSenha.addEventListener("blur", animSenhaCancel);
+ 
+ 
 
-
-
-
+//animação ao clicar em cadastrar-se//
 function animForm(){
-    var forms = document.querySelectorAll("div");
-
+    //animando o retângulo vermelho
+    var retanguloRed = document.querySelectorAll("div");
+ 
     var b = 400
     clearInterval(id5)
     var id5 = setInterval(frame0, 1)
-
+ 
     function frame0(){
         if (b >= 820){
             clearInterval(id5)
         }
-
+ 
         else{
             b = b + 7
-            forms[0].style.width = b + "px"
+            retanguloRed[0].style.width = b + "px"
         }
     }
-
+ 
     var p = 400
     clearInterval(id6)
     var id6 = setInterval(frame, 1)
-
+ 
     function frame(){
         if (p == 500){
             clearInterval(id6)
         }
-
+ 
         else{
             p++
-            forms[0].style.height = p + "px"
+            retanguloRed[0].style.height = p + "px"
         }
     }
 
-    document.getElementById("papas").style.display = "block"
-}
+    //Fazendo o papas aparecer em maios ou menos 1 segundo//
+    setTimeout(function(){document.getElementById("papas").style.display = "inline"}, 980)
+    
+    //Fazendo o papas andar e desaparecer
+    var c = 300
+    clearInterval(id7)
+    var id7 = setInterval(frame1, 3)
+    function frame1(){
+        if (c == 1300){
+            clearInterval(id7)
+            document.getElementById("papas").style.display = "none"
+        }
+ 
+        else{
+            c++
+            document.getElementById("papas").style.left = c + "px"
+        }
 
-var form1 = document.getElementById("login");
+    }
+
+    //animando o form como um todo//
+    var forms = document.getElementById("login")
+    var a = 90
+    var id8 = setInterval(frame2, 1)
+
+    function frame2(){
+        if (a == -50){
+            clearInterval(id8)
+        }
+ 
+        else{
+            a--
+            forms.style.left = a + "px"
+        }
+        
+        var inputs = document.querySelectorAll("input")
+        var labels = document.querySelectorAll("label")
+        var imgs = document.querySelectorAll("img")
+
+        inputs[0].style.width = 230 + "px"
+        inputs[1].style.width = 230 + "px"
+
+        labels[0].style.right = 110 + "px"
+        labels[1].style.right = 110 + "px"
+
+        imgs[0].style.left = 350 + "px"
+        imgs[1].style.left = 350 + "px"
+    }
+
+}
+ 
+var form1 = document.getElementById("cadastre-se");
 form1.addEventListener("click", animForm)
+ 
