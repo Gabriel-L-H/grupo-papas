5ec031617c64f1cfabb717bff87c1818e1b57d76
RafaelTosti <rafaeltosti09@gmail.com>
2024-09-16 11:25:45 -0300

    Delete Check-out directory

--- Check-out/comprar.js
+++
@@ -1,9 +0,0 @@
-function val_compra() {
- var ccpf = parseInt (document.getElementById('cpf')).value;
- var ceep  = parseInt (document.getElementById('numericInput').addEventListener('cep')).value;
-  if(ccpf == isNaN && ceep == isNaN ){
-    alert('Somente números')
-  } else {
-    alert('Compra concluída')
-  }
-}
